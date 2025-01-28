c9cad80894c002c15f548d7321f4b995ed7fb0f5
AbdalrahmanAn <abdalrahmannasr334@gmail.com>
2025-01-28 06:55:41 +0200

    Update project 2

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '6bdb83f10a1cc551fa27');
+<?php return array('dependencies' => array('jquery'), 'version' => '5e2467f59ab471ed375c');
--- src/modules/Search.js
+++ src/modules/Search.js
@@ -3,6 +3,7 @@ import $ from 'jquery';
 class search {
     // 1. create our object (ده المكان اللي بننشئ فيه الكائن او الكلاس)
     constructor() {
+        this.addSearchHtml();
         this.resultDiv = $("#search-overlay__results");
         this.openButton = $(".js-search-trigger");
         this.closeButton = $(".search-overlay__close");
@@ -32,18 +33,32 @@ class search {
                     this.resultDiv.html('<div class="spinner-loader"></div>');
                     this.isSpinnerVisible = true;
                 }
-                this.typingTimer = setTimeout(this.getResult.bind(this), 2000);
+                this.typingTimer = setTimeout(this.getResult.bind(this), 500);
             }else{
                 this.resultDiv.html('');
                 this.isSpinnerVisible = false;
             }
         }
-        this.previousValue = this.searchField.val();
+        this.previousValue = this.searchField.val();  
     }
 
     getResult() {
-        this.resultDiv.html("imagine real search results here...");
-        this.isSpinnerVisible = false;
+        $.when(
+            $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
+            $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val()),
+            $.getJSON(universityData.root_url + '/wp-json/wp/v2/program?search=' + this.searchField.val())
+    ).then((posts, pages, programs) => {
+            var combinedResults = posts[0].concat(pages[0], programs[0]);
+            this.resultDiv.html(`
+                <h2 class="search-overlay__section-title">General Information</h2>
+                ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No General Information Matches That Search</p>' }
+                ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a><li>`).join('')}
+                ${combinedResults.length ? '</ul>' : ''}
+                `);
+            this.isSpinnerVisible = false;
+        }, () => {
+            this.resultDiv.html('<p>Unexpected error; Please try again.</p>');
+        })
     }
 
     keyPressDispatcher(e) {
@@ -60,15 +75,37 @@ class search {
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
         $("body").addClass("body-no-scroll");
+        this.searchField.val('');
+        setTimeout(() => this.searchField.focus(), 301);
         this.isOverlayOpen = true;
     }
 
     closeOverlay() {
         this.searchOverlay.removeClass("search-overlay--active");
         $("body").removeClass("body-no-scroll");
+        
         this.isOverlayOpen = false;
 
     }
+
+    addSearchHtml() {
+        $('body').append(`
+    <div class="search-overlay">
+        <div class="search-overlay__top">
+            <div class="container">
+                <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+                <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+            </div>
+        </div>
+
+        <div class="container">
+            <div id="search-overlay__results"></div>
+        </div>
+    </div>
+        `)
+    }
+
 }
 
 export default search
